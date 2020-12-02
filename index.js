35b238961f44a064a80c6f640a1957e5273d80d3
Max Lechner <mkonlech@gmail.com>
2020-12-01 19:01:52 -0800

    updating functions

--- index.js
+++ index.js
@@ -1,16 +1,60 @@
 // array of questions for user
-const questions = [
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require("./utils/generateMarkdown");
 
+const questions = [
+    {
+        name: 'title',
+        type: 'input',
+        message: 'What is the title of your project.',
+    },
+    {
+        name: 'description',
+        type: 'input',
+        message: 'Describe your project in detail.',
+    },
+    {
+        name: 'install',
+        type: 'input',
+        message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
+    },
+    {
+        name: 'usage',
+        type: 'input',
+        message: 'Provide instructions and examples for use.',
+    },
+    {
+        name: 'credits',
+        type: 'input',
+        message: 'List your collaborators, if any, with links to their GitHub profiles. If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section. If you followed tutorials, include links to those here as well.'
+    },
+    {
+        type: 'list',
+        message: 'Select the license',
+        name: 'license',
+        choices: ['MIT','GPLv3','GPL', 'Apache'],
+    },
 ];
+ 
 
 // function to write README file
 function writeToFile(fileName, data) {
-}
+    fs.writeFile(fileName, data, function (error){
+        if (error) {
+            throw error;
+        }
+    })
+};
 
 // function to initialize program
 function init() {
-
-}
+    inquirer.prompt(questions).then((response) =>{
+        console.log(response)
+        let readme = generateMarkdown(response);
+        writeToFile("./README.md", readme);
+    })
+};
 
 // function call to initialize program
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,20 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  #${data.title}   License: (https://img.shields.io/badge/License-${data.license}-blue.svg)](https://lbesson.mit-license.org/)
+
+  #Description
+  ${data.description}
+
+  #Installation
+  ${data.install}
+
+  #Usage
+  ${data.usage}
+
+  #Credits
+  ${data.credits}
+
 
 `;
 }
